be8b4b8835b6a03580a0530a158fc9325be2960d
StefanBerlin <rupp.stef@googlemail.com>
2014-06-23 16:33:47 +0200

    expanded core to use plugin, exchange events

--- core_model_test/core.py
+++ core_model_test/core.py
@@ -2,8 +2,9 @@ __author__ = 'control'
 
 
 from yapsy.PluginManager import PluginManager
-from multiprocessing import Process, Array
-
+from multiprocessing import Process, Value, Array, Lock, Queue
+import time
+import os
 
 def main():
     # Load the plugins from the plugin directory.
@@ -11,22 +12,32 @@ def main():
     manager.setPluginPlaces(["plugins"])
     manager.collectPlugins()
 
+    lock = Lock()
 
-
-    sharedArr = Array('i',range(10))
+    eventQueue = Queue()
+    sharedArr = Array('i',range(6))
 
     # Loop round the plugins and start them
     for plugin in manager.getAllPlugins():
-        plugin.plugin_object.start_plugin(sharedArr)
+        plugin.plugin_object.start_plugin(sharedArr, eventQueue,lock)
 
 
-
-
-
-
-
-
-
+    print ('Core process id:', os.getpid())
+    goOn = 1;
+    while (goOn):
+        event=eventQueue.get()
+        if (event==1):
+            #stop
+            goOn = 0
+            for plugin in manager.getAllPlugins():
+                plugin.plugin_object.end_plugin()
+        if (event==2):
+            #new data
+            print("---------------------")
+            lock.acquire()
+            for i in range(6):
+                print(sharedArr[i])
+            lock.release()
 
 
 if __name__ == "__main__":
--- core_model_test/plugins/plugin1.py
+++ core_model_test/plugins/plugin1.py
@@ -2,18 +2,41 @@ __author__ = 'control'
 
 
 from yapsy.IPlugin import IPlugin
-from multiprocessing import Process, Array
+from multiprocessing import Process, Value, Array
 import os
+import time
 
 
 
 class PluginOne(IPlugin):
-    def pro(self,l):
+    def pro(self,l,goOn, eventQueue,lock):
         print("Plugin1 Process started")
-        while(1):
-          l[2] = -1
-          #print(l[2])
+        i = 0
+        print ('process id:', os.getpid())
+        while(goOn.value==1):
+            lock.acquire()
+            for n in range(len(l)):
+                l[n] = i + n
+            time.sleep(2)
+            lock.release()
+            i += 1
+            if (i % 2):
+                 eventQueue.put(2)
+            if ( i > 7 ):
+                 eventQueue.put(1)
 
-    def start_plugin(self,l):
-        p = Process(target=self.pro, args=(l,) )
-        p.start()
+
+
+
+    def __init__(self):
+        self.goOn  = Value('i',0)
+
+    def start_plugin(self,l,eventQueue,lock):
+        self.goOn.value =1
+        self.p = Process(target=self.pro, args=(l,self.goOn, eventQueue,lock) )
+        self.p.start()
+
+    def end_plugin(self):
+        self.goOn.value =0
+        self.p.join()
+        print("Plugin joined")
