bb6902f47ac9682ce3cd3121640f74624e2864b2
StefanBerlin <rupp.stef@googlemail.com>
2014-06-25 12:02:12 +0200

    added a new testing modul for core, where communication of two plugins is tested

---
+++ core_model_test_multiple_plugin_communication/plugins/plugin1.py
@@ -0,0 +1,37 @@
+__author__ = 'control'
+
+
+from yapsy.IPlugin import IPlugin
+from multiprocessing import Process, Value, Array
+import os
+import time
+
+
+
+class PluginOne(IPlugin):
+    def start_plugin(self,l,eventQueue,lock,plQueue):
+        print('Plugin1 started')
+        goOn = 1
+        i = 0;
+        while(goOn):
+            try:
+                event=plQueue.get_nowait()
+                if (event == 1):
+                    goOn=0
+                    eventQueue.put([1, 1])
+                    break
+            except:
+                time.sleep(0)
+
+            lock.acquire()
+            for n in range(len(l)):
+                l[n] = i + n
+            lock.release()
+            time.sleep(0.1)
+            i += 1
+            if (i % 2):
+                 eventQueue.put([1, 2])
+            if i > 10:
+                goOn = 0
+                eventQueue.put([1,1])
+
