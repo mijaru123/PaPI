c358d54c79a217ac84ac08b0f2ad1e7405ff370f
StefanBerlin <rupp.stef@googlemail.com>
2014-06-27 14:05:11 +0200

    prototype core

--- papi/core.py
+++ papi/core.py
@@ -1,17 +1,92 @@
-#!/usr/bin/python3
+__author__ = 'control'
 
-version = '0.1'
 
-def start(foo, bar=True):
-    """
-    Function used to start PaPI.
+from yapsy.PluginManager import PluginManager
+from multiprocessing import Process, Value, Array, Lock, Queue
+import time
+import os
+import gui
 
-    :param foo: used to foo
-    :type foo: String
-    :param bar: used to bar
-    :type bar: bool
-    :rtype bool
+def main():
+    # for better process tracking, print process ID of core process
+    print('Core process id:', os.getpid())
 
-    """
-    print("start PaPI - Plugin based Process Interaction")
-    return True
+    # Load the plugins from the plugin directory.
+    # prepare plugin manager for dir 'plugins'
+    manager = PluginManager()
+    manager.setPluginPlaces(["plugins"])
+    manager.collectPlugins()
+
+
+    # create a lock object for mutex usage
+    lock = Lock()
+
+    # event queue of plugin1 for getting events from core process or gui
+    IOPQueue = Queue()
+    CoreQueue = Queue()
+    GUIQueue = Queue()
+
+
+    # shared memory array, represents buffer in core process
+    sharedArr_time = Array('d',range(100))
+    sharedArr_value = Array('d',range(100))
+
+
+
+    plugin = manager.getPluginByName('IOP1')
+    IOPProcess =  Process(target=plugin.plugin_object.start_plugin, args=(CoreQueue,IOPQueue,sharedArr_time,sharedArr_value,lock) )
+    IOPProcess.start();
+
+
+
+    #GUIProcess = Process(target=startGUI  , args=(CoreQueue,GUIQueue,sharedArr_time,sharedArr_value,lock))
+    #GUIProcess.start()
+
+    # loop for core to wait and polling event queue
+    # event definition for core:
+    # [x,1]: plugin will quit, please join process -> in this example 1 pl quits means that core will end
+    # [x,2]: plugin placed new data in the buffer. please collect
+    goOn = 1;
+    IOPalive = 1;
+    GUIalive = 1;
+
+    while goOn:
+        # blocking till there is a element to get form queue
+        # blocking is no active waiting -> low cpu usage
+        event = CoreQueue.get()
+        #print("Core: got event ",event[1]," from plugin ", event[0])
+        if event[0] == 'IOP':
+            if event[1] == 'Data':
+                # new Data available, notice GUI
+                GUIQueue.put(['Core','Data'])
+
+            if event[1] == 'EndJoin':
+                # IOP will end, join process
+                IOPProcess.join()
+                IOPalive = 0
+
+            if event[1] == 'Join':
+                # IOP needs join
+                IOPProcess.join()
+                IOPalive = 0
+
+        if event[0] == 'GUI':
+            if event[1] == 'Join':
+                # join the GUI
+                # GUIProcess.join()
+                GUIalive = 0
+
+            if event[1] == 'EndJoin':
+                # GUI asks for END
+                IOPQueue.put(['Core','End'])
+                # GUIProcess.join()
+                GUIalive = 0
+
+        goOn = GUIalive | IOPalive
+
+    # prints this debug message when core process finished/exits
+    print("Core finished")
+
+
+if __name__ == "__main__":
+    main()
